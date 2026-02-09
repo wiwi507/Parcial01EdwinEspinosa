646f1a46008b21cc3ee119c958572c49c7ee0abd
wiwi507 <146676574+wiwi507@users.noreply.github.com>
2026-02-09 09:40:16 -0500

    Update  Edwin EspinosaFuncionG2-Suma-lista.py
    Fusione ambos archivos

--- FuncionG2-Suma-lista.py
+++ FuncionG2-Suma-lista.py
@@ -11,3 +11,21 @@ def suma_lista(lista):
 numeros = [10, 20, 30, 40, 50]
 resultado = suma_lista(numeros)
 print("Eduardo Salinas - Suma total:", resultado)
+
+#Edwin Espinosa 8-1023-293
+def calcular_promedio(lista):
+    if not lista:  # Verificamos que la lista no esté vacía para evitar error de división por cero
+        return 0
+    
+    suma_total = sum(lista)
+    cantidad_elementos = len(lista)
+    promedio = suma_total / cantidad_elementos
+    
+    return promedio
+
+# Ejemplo de uso:
+mis_numeros = [10, 8, 9, 7, 10]
+resultado = calcular_promedio(mis_numeros)
+
+print(f"La lista es: {mis_numeros}")
+print(f"El promedio es: {resultado}")
